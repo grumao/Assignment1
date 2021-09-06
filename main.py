be4364d449cbfbb05b5c11dfc0795abc4ce8baa4
grumao <grumao@stevens.edu>
2021-09-06 09:24:52 -0400

    Homework1 completed

---
+++ main.py
@@ -0,0 +1,15 @@
+# Assignment1: plot sine and cosine functions on the same plot using matplotlib package.
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.arange(0,4*np.pi,0.1)   # start,stop,step
+y = np.sin(x)
+z = np.cos(x)
+
+plt.plot(x,y,x,z)
+plt.xlabel('x values from 0 to 4pi')  # string must be enclosed with quotes '  '
+plt.ylabel('sin(x) and cos(x)')
+plt.title('Plot of sin and cos from 0 to 4pi')
+plt.legend(['sin(x)', 'cos(x)'])      # legend entries as seperate strings in a list
+plt.show()
+
